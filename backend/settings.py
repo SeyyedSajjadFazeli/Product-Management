8a904c91c03d0e53335b246e662588bbba099acc
SeyyedSajjadFazeli <sajjadfazeli2009@gmail.com>
2026-07-13 20:57:48 +0330

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-u2eal+j^o(5jd$$$+-gskn+n85-c7kq^mqa5p$ey%7_znde6^*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
